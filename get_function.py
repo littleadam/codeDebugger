7423f27d92ee9ec224afab8adbf504b0b87cc14e
ARUL DORAISWAMY <arulmuruga.ad@gmail.com>
2023-04-24 22:57:55 +0530

    Update get_function.py

--- get_function.py
+++ get_function.py
@@ -1,3 +1,13 @@
+from pycparser import c_parser, c_ast
+
+def check_c_syntax(statement):
+    try:
+        parser = c_parser.CParser()
+        ast = parser.parse(statement)
+        return True
+    except c_parser.ParseError:
+        return False
+
 def print_lines_with_keywords(file_path, keywords):
     with open(file_path, 'r') as f:
         contents = f.read()
